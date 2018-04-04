6abd91dd2d5bcd589726c76a54d679ad0d594860
Dariusz Paluch <dariusz@dariuszpaluch.com>
2018-04-04 19:48:07 +0200

    Fix bug in TestList

--- src/modules/Tests/TestList.js
+++ src/modules/Tests/TestList.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
-import classnames from 'classnames';
 import { connect } from 'react-redux';
 
 import { getTestsRequest } from 'modules/Tests/utils/actions';
-import Card from 'libs/ui/Card/Card';
 import { getTests, getTestsIds } from 'modules/Tests/utils/getters';
 import { injectIntl } from 'react-intl';
 import messages from 'modules/Tests/utils/messages';
 import TestDescriptionCard from 'modules/Tests/components/TestDescriptionCard/TestDescriptionCard';
-import Button from 'libs/ui/Button/Button';
-import IconButton from 'libs/ui/IconButton/IconButton';
 import icons from 'consts/icons';
-import Icon from 'libs/ui/Icon/Icon';
 import FloatButton from 'libs/ui/FloatButton/FloatButton';
 import MainLayout from 'modules/MainLayout/MainLayout';
 import { Link, withRouter } from 'react-router-dom';
@@ -45,21 +38,23 @@ class TestList extends Component {
       <MainLayout
         appBarTittle={intl.formatMessage(messages.TESTS_LIST_HEADER)}
       >
-        {testsIds.map((testId, index) => {
-          const test = tests[testId];
+        <div>
+          {testsIds.map((testId, index) => {
+            const test = tests[testId];
 
-          return (
-            <Link
-              key={test.id || index}
-              to={`/tests/${testId}`}
-            >
-              <TestDescriptionCard
-                test={test}
-              />
-            </Link>
+            return (
+              <Link
+                key={test.id || index}
+                to={`/tests/${testId}`}
+              >
+                <TestDescriptionCard
+                  test={test}
+                />
+              </Link>
 
-          )
-        })}
+            )
+          })}
+        </div>
         <FloatButton icon={icons.ADD} onClick={this.onClickAddTest}/>
       </MainLayout>
     );
