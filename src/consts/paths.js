58c21aa642099a3b184c89f89dfd13682328ec39
Dariusz Paluch <dariusz@dariuszpaluch.com>
2018-04-12 18:31:56 +0200

    Change paths

--- src/consts/paths.js
+++ src/consts/paths.js
@@ -1,14 +1,14 @@
 
 const paths = {
   INDEX: `${PUBLIC_PATH}`,
-  SIGN_IN: `${PUBLIC_PATH}/sign-in`,
-  SIGN_UP: `${PUBLIC_PATH}/sign-up`,
-  DASHBOARD: `${PUBLIC_PATH}/dashboard`,
-  MAKE_TEST: `${PUBLIC_PATH}/make-test/:testId`,
-  QUESTIONS: `${PUBLIC_PATH}/question`,
-  TESTS: `${PUBLIC_PATH}/tests`,
-  SETTINGS: `${PUBLIC_PATH}/settings`,
-  TESTS_RESULTS: `${PUBLIC_PATH}/tests-results`,
+  SIGN_IN: `${PUBLIC_PATH}sign-in`,
+  SIGN_UP: `${PUBLIC_PATH}sign-up`,
+  DASHBOARD: `${PUBLIC_PATH}dashboard`,
+  MAKE_TEST: `${PUBLIC_PATH}make-test/:testId`,
+  QUESTIONS: `${PUBLIC_PATH}question`,
+  TESTS: `${PUBLIC_PATH}tests`,
+  SETTINGS: `${PUBLIC_PATH}settings`,
+  TESTS_RESULTS: `${PUBLIC_PATH}tests-results`,
 };
 
 export default paths;
