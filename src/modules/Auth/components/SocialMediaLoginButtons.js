8edbc89ae810289a16ec0ce0fee0cc66c50d5f1a
Dariusz Paluch <dariusz@dariuszpaluch.com>
2018-06-16 08:36:47 +0200

    fix returnUrl

--- src/modules/Auth/components/SocialMediaLoginButtons.js
+++ src/modules/Auth/components/SocialMediaLoginButtons.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classnames from 'classnames';
-import { Link } from 'react-router-dom';
 import Button from 'libs/ui/Button/Button';
 import { API_URL } from 'settings';
 
@@ -16,23 +15,21 @@ export default class SocialMediaLoginButtons extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.returnUrl = window.document.location.href + location.search;
   }
 
   render() {
-    const returnUrl = window.document.location.href + location.search;
-
     return (
       <div className="social-media-buttons">
         <a
           key="facebook"
-          href={`${API_URL}/auth/facebook?returnUrl=${encodeURIComponent(returnUrl)}`}
+          href={`${API_URL}/auth/facebook?returnUrl=${encodeURIComponent(this.returnUrl)}`}
         >
           <Button className="social-login login-facebook">Login with Facebook</Button>
         </a>
         <a
           key="google"
-          href={`${API_URL}/auth/google?returnUrl=${encodeURIComponent(returnUrl)}`}
+          href={`${API_URL}/auth/google?returnUrl=${encodeURIComponent(this.returnUrl)}`}
         >
           <Button className="social-login login-google">Login with Google</Button>
         </a>
